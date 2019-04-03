f2d12365fb7e7955b5d0a939dfc2e4c3d58529e5
JanBosNL <48719788+JanBosNL@users.noreply.github.com>
2019-04-03 18:55:41 +0200

    Update 5_strips_multi_lenght_fastled_fire_demo.ino

--- 5_strips_multi_lenght_fastled_fire_demo.ino
+++ 5_strips_multi_lenght_fastled_fire_demo.ino
@@ -1,27 +1,33 @@
-#include <FastLED.h>
+//#define FASTLED_ESP8266_NODEMCU_PIN_ORDER // Only use when using nodeMCU pin numbers like D4,D5,D12,D15 etc...
 
-#define LED_PIN_0     12 //288
-#define LED_PIN_1     14 //30
-#define LED_PIN_2     13 //30
-#define LED_PIN_3     4 //30
-#define LED_PIN_4     5 //30
-#define CLOCK_PIN   15
-#define COLOR_ORDER BGR
-#define CHIPSET     APA102
-#define NUM_LEDS_0    288
-#define NUM_LEDS_1    288
-#define NUM_LEDS_2    288
-#define NUM_LEDS_3    30
-#define NUM_LEDS_4    30
+#include <FastLED.h> // I am using the real GPIO numbers, not the D# nodeMCU numbers. If you want to use the NODEMCU specific numbers you need to define (#define FASTLED_ESP8266_NODEMCU_PIN_ORDER)
+                     // Before including (#include <FastLED.h>)
+
+#define LED_PIN_0     12 //288 Here I define the pins per ledstrip 
+#define LED_PIN_1     13 //288 Not all pins are unused so be careful
+#define LED_PIN_2     14 //288 to choose the ones that are availeable on the NODEMCU
+#define LED_PIN_3     4  //30    
+#define LED_PIN_4     5  //30    
+#define CLOCK_PIN     15 //     Here I define the single clockpin So dont use a clockpin per ledstrip. Al the clock wires from all the strips use this single clockpin.
+#define COLOR_ORDER BGR  //     Defining the colour order of the 3 colours the APA102 led has. Other choices can be BRG, RGB, RBG, GRB, GBR 
+#define CHIPSET     APA102 // Defining the actual LED chip used that Fastled can recognise. 
+#define NUM_LEDS_0    288 // Define nr of leds on the strip connected to pin 12 
+#define NUM_LEDS_1    288 // Define nr of leds on the strip connected to pin 13
+#define NUM_LEDS_2    288 // Define nr of leds on the strip connected to pin 14
+#define NUM_LEDS_3    30  // Define nr of leds on the strip connected to pin 4
+#define NUM_LEDS_4    30  // Define nr of leds on the strip connected to pin 5
 #define NUM_STRIPS 5 // Total number of strips
 
-#define BRIGHTNESS  50
-#define FRAMES_PER_SECOND 67
+#define MILLI_AMPS         30000 // IMPORTANT!!: set the max milli-Amps of your powersupply (30A = 30000mA) as my powersupply is rated up to 30A.
+                                 // Fastled calculates power consumption by using known consumption specs per led and chipset, to lower brightness when 
+                                 // you try and draw more power then your powersupply can handle. This to not damage your electronics.
+
+#define BRIGHTNESS  200        // Overall Brightnes from 0 up to 255
+#define FRAMES_PER_SECOND 39   // Speed of the frames. Higher number is more speed.
 
 bool gReverseDirection = false;
 
-CRGB leds[NUM_LEDS_0 + NUM_LEDS_1 + NUM_LEDS_2 + NUM_LEDS_3 + NUM_LEDS_4];  // Add all together here
-
+CRGB leds[NUM_LEDS_0 + NUM_LEDS_1 + NUM_LEDS_2 + NUM_LEDS_3 + NUM_LEDS_4];  // Add all the leds together here
 
 uint8_t stripLength; // Used to store a strip length.
 
@@ -36,6 +42,34 @@ void setup() {
   FastLED.setBrightness( BRIGHTNESS );
   
 }
+
+//Below is code I used from FASTLED EXAMPLES
+// Fire2012 with programmable Color Palette
+//
+// This code is the same fire simulation as the original "Fire2012",
+// but each heat cell's temperature is translated to color through a FastLED
+// programmable color palette, instead of through the "HeatColor(...)" function.
+//
+// Four different static color palettes are provided here, plus one dynamic one.
+// 
+// The three static ones are: 
+//   1. the FastLED built-in HeatColors_p -- this is the default, and it looks
+//      pretty much exactly like the original Fire2012.
+//
+//  To use any of the other palettes below, just "uncomment" the corresponding code.
+//
+//   2. a gradient from black to red to yellow to white, which is
+//      visually similar to the HeatColors_p, and helps to illustrate
+//      what the 'heat colors' palette is actually doing,
+//   3. a similar gradient, but in blue colors rather than red ones,
+//      i.e. from black to blue to aqua to white, which results in
+//      an "icy blue" fire effect,
+//   4. a simplified three-step gradient, from black to red to white, just to show
+//      that these gradients need not have four components; two or
+//      three are possible, too, even if they don't look quite as nice for fire.
+//
+// The dynamic palette shows how you can change the basic 'hue' of the
+// color palette every time through the loop, producing "rainbow fire".
 
 void loop()
 {
