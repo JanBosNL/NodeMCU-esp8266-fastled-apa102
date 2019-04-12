c6bad0bb572be3622e17aac428031884449aa36b
JanBosNL <48719788+JanBosNL@users.noreply.github.com>
2019-04-12 12:01:57 +0200

    Updated custum 8 colour fire palette
    Cleaned up code some more and added 8 colour custum fire palette.

--- 5_strips_multi_lenght_fastled_fire_demo.ino
+++ 5_strips_multi_lenght_fastled_fire_demo.ino
@@ -22,14 +22,26 @@
                                  // Fastled calculates power consumption by using known consumption specs per led and chipset, to lower brightness when 
                                  // you try and draw more power then your powersupply can handle. This to not damage your electronics.
 
-#define BRIGHTNESS  200        // Overall Brightnes from 0 up to 255
-#define FRAMES_PER_SECOND 39   // Speed of the frames. Higher number is more speed.
+#define BRIGHTNESS  180       // Overall Brightnes from 0 up to 255
+#define FRAMES_PER_SECOND 120   // Speed of the frames. Higher number is more speed.
+#define COOLING  40 // 20/100
+#define SPARKING 25 // 50/200
 
 bool gReverseDirection = false;
 
 CRGB leds[NUM_LEDS_0 + NUM_LEDS_1 + NUM_LEDS_2 + NUM_LEDS_3 + NUM_LEDS_4];  // Add all the leds together here
 
-uint8_t stripLength; // Used to store a strip length.
+// Create my custom, 8 color gradient, Fire palette!
+DEFINE_GRADIENT_PALETTE( myfire_gp ) {
+  0,    0,0,0,            //BLACK
+ 36,    36,0,0,          //MAROON
+ 72,    51,26,0,         //Brown
+108,    140,39,0,         //ORANGERED
+144,    255,165,0,        //ORANGE
+180,    255,215,30,        //AMBER
+216,    255,255,0,        //YELLOW
+255,    255,255,255 };    //WHITE
+CRGBPalette16 myPal = myfire_gp;    
 
 void setup() {
   delay(3000); // sanity delay
@@ -40,84 +52,50 @@ void setup() {
   FastLED.addLeds<CHIPSET, LED_PIN_4, CLOCK_PIN, COLOR_ORDER>(leds, NUM_LEDS_4).setCorrection( TypicalLEDStrip );
   
   FastLED.setBrightness( BRIGHTNESS );
-  
+
 }
 
 //Below is code I used from FASTLED EXAMPLES
 // Fire2012 with programmable Color Palette
-//
-// This code is the same fire simulation as the original "Fire2012",
-// but each heat cell's temperature is translated to color through a FastLED
-// programmable color palette, instead of through the "HeatColor(...)" function.
-//
-// Four different static color palettes are provided here, plus one dynamic one.
-// 
-// The three static ones are: 
-//   1. the FastLED built-in HeatColors_p -- this is the default, and it looks
-//      pretty much exactly like the original Fire2012.
-//
-//  To use any of the other palettes below, just "uncomment" the corresponding code.
-//
-//   2. a gradient from black to red to yellow to white, which is
-//      visually similar to the HeatColors_p, and helps to illustrate
-//      what the 'heat colors' palette is actually doing,
-//   3. a similar gradient, but in blue colors rather than red ones,
-//      i.e. from black to blue to aqua to white, which results in
-//      an "icy blue" fire effect,
-//   4. a simplified three-step gradient, from black to red to white, just to show
-//      that these gradients need not have four components; two or
-//      three are possible, too, even if they don't look quite as nice for fire.
-//
-// The dynamic palette shows how you can change the basic 'hue' of the
-// color palette every time through the loop, producing "rainbow fire".
 
 void loop()
 {
   // Add entropy to random number generator; we use a lot of it.
 random16_add_entropy( random(256));
 
-  Fire2012(); // run simulation frame
+  Fire2012WithPalette(); // run simulation frame
   
   FastLED.show(); // display this frame
   FastLED.delay(1000 / FRAMES_PER_SECOND);
 }
-
-
-
-#define COOLING  55 // 20/100
-
-#define SPARKING 15 // 50/200
-
-
-void Fire2012()
-{
-// Array of temperature readings at each simulation cell
+void Fire2012WithPalette()
+{ 
+ // Array of temperature readings at each simulation cell
   static byte heat[NUM_LEDS_0];
-  
 
   // Step 1.  Cool down every cell a little
     for( int i = 0; i < NUM_LEDS_0; i++) {
       heat[i] = qsub8( heat[i],  random8(0, ((COOLING * 10) / NUM_LEDS_0) + 2));
     }
-
-  
   
     // Step 2.  Heat from each cell drifts 'up' and diffuses a little
-    for( int k= NUM_LEDS_0 - 1; k >= 3; k--) {
+    for( int k= NUM_LEDS_0 - 1; k >= 2; k--) {
       heat[k] = (heat[k - 1] + heat[k - 2] + heat[k - 2] ) / 3;
     }
-
-   
+    
+    
     // Step 3.  Randomly ignite new 'sparks' of heat near the bottom
     if( random8() < SPARKING ) {
-      int y = random8(9);
+      int y = random8(7);
       heat[y] = qadd8( heat[y], random8(160,255) );
     }
 
-   
     // Step 4.  Map from heat cells to LED colors
     for( int j = 0; j < NUM_LEDS_0; j++) {
-      CRGB color = HeatColor( heat[j]);
+      // Scale the heat value from 0-255 down to 0-240
+      // for best results with color palettes.
+      byte colorindex = scale8( heat[j], 240);
+      CRGB color = ColorFromPalette( myPal, colorindex);
       int pixelnumber;
       if( gReverseDirection ) {
         pixelnumber = (NUM_LEDS_0-1) - j;
@@ -125,8 +103,17 @@ void Fire2012()
         pixelnumber = j;
       }
       leds[pixelnumber] = color;
+      addGlitter(1);
     }
-
-
-    
 }
+
+//Sparks at the top flying of in the sky
+void addGlitter( fract8 chanceOfGlitter) 
+{
+if( random8() < chanceOfGlitter) {
+leds[ random16(NUM_LEDS_0) ] += CRGB (140,39,0); //Orangered 
+  }
+
+
+}
+
